eb2dbaf823a3d8a14f105881c942f19edda45a30
Shilpa <kambleshilpa216@gmail.com>
2021-10-06 13:57:08 +0530

    Selected successfully

---
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -0,0 +1,10 @@
+package com.qa.pages;
+
+public class LoginPage {
+	
+	public void loginmethod() {
+		System.out.println("login method");
+	}
+	
+}
+
---
+++ src/test/java/com/qa/testpages/LoginPageTest.java
@@ -0,0 +1,11 @@
+package com.qa.testpages;
+
+import org.testng.annotations.Test;
+
+public class LoginPageTest {
+
+	@Test
+	public void loginpagetest() {
+		System.out.println("login page test");
+	}
+}
